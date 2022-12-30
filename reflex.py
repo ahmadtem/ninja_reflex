ef4ef1273ed2c9e789d5c0a6de1505860d04cb63
Ahmad M. Temoor <ahmadtem@buffalo.edu>
2022-12-29 17:04:54 -0700

    Update reflex.py
    This was a fun project. This commit has the final code that was written for this project. It worked perfectly on the first attempt!

--- reflex.py
+++ reflex.py
@@ -1,35 +1,58 @@
-import time
-import random
 from gpiozero import LightSensor
 from time import sleep
 import pigpio
+import time
+import random
+import os
 
+os.system("sudo pigpiod") 
 
+sensitivity = 0.15
+strength = 0.7
+len_factor = 10
 
 pi = pigpio.pi()
 
+ldr1 = LightSensor(23)
+ldr2 = LightSensor(25)
+ldr3 = LightSensor(27)
+ldr4 = LightSensor(4)
+
 #R, G, B
-wall_1_pins = [17, 22, 24]
-wall_2_pins = [26, 19, 21]
+wall_1_pins = [22, 17, 24]
+wall_2_pins = [6, 13, 5]
 wall_3_pins = [16, 12, 20]
-wall_4_pins = [6, 5, 13]
+wall_4_pins = [26, 19, 21]
 
 
-
-def LED_on(wall_pins):
+def LED_on(wall_pins, R_max, G_max, B_max):
 
 	RED_PIN = wall_pins[0]
 	GREEN_PIN = wall_pins[1]
 	BLUE_PIN = wall_pins[2]
 
-	r = 255.0
-	g = 255.0
-	b = 255.0
+	r = random.randint(R_max,R_max)
+	g = random.randint(G_max,G_max)
+	b = random.randint(B_max,B_max)
 
 	setLights(RED_PIN, r)
 	setLights(GREEN_PIN, g)
 	setLights(BLUE_PIN, b)
 
+
+def random_LED_on(wall_pins):
+
+	RED_PIN = wall_pins[0]
+	GREEN_PIN = wall_pins[1]
+	BLUE_PIN = wall_pins[2]
+
+	r = random.randint(0,255)
+	g = random.randint(0,255)
+	b = random.randint(0,255)
+
+	setLights(RED_PIN, r)
+	setLights(GREEN_PIN, g)
+	setLights(BLUE_PIN, b)
 
 
 def LED_off(wall_pins):
@@ -48,18 +71,18 @@ def LED_off(wall_pins):
 
 
 
-def light_up(wall_number):
+def random_light_up(wall_number):
 	if wall_number == 1:
-		LED_on(wall_1_pins)
+		random_LED_on(wall_1_pins)
 
 	elif wall_number == 2:
-		LED_on(wall_2_pins)
+		random_LED_on(wall_2_pins)
 
 	elif wall_number == 3:
-		LED_on(wall_3_pins)
+		random_LED_on(wall_3_pins)
 
 	elif wall_number == 4:
-		LED_on(wall_4_pins)	
+		random_LED_on(wall_4_pins)	
 
 
 
@@ -77,71 +100,158 @@ def light_off(wall_number):
 		LED_off(wall_4_pins)	
 
 
+
+def wall_hit(wall_number, sensitivity):
+	if wall_number == 1:
+		while True:
+			if ldr1.value < sensitivity:
+				return True
+
+	elif wall_number == 2:
+		while True:
+			if ldr2.value < sensitivity:
+				return True
+
+	elif wall_number == 3:
+		while True:
+			if ldr3.value < sensitivity:
+				return True
+
+	elif wall_number == 4:
+		while True:
+			if ldr4.value < sensitivity:
+				return True
+
+
+def laser_aligned(wall_number, strength):
+	if wall_number == 1:
+		while True:
+			if ldr1.value > strength:
+				return True
+
+	elif wall_number == 2:
+		while True:
+			if ldr2.value > strength:
+				return True
+
+	elif wall_number == 3:
+		while True:
+			if ldr3.value > strength:
+				return True
+
+	elif wall_number == 4:
+		while True:
+			if ldr4.value > strength:
+				return True
+
+
 def setLights(pin, brightness):
 
 	bright = 255
 	realBrightness = int(int(brightness) * (float(bright) / 255.0))
 	pi.set_PWM_dutycycle(pin, realBrightness)
-
-
+	
 
 def light_all():
-	LED_on(wall_1_pins)
-	LED_on(wall_2_pins)
-	LED_on(wall_3_pins)
-	LED_on(wall_4_pins)
+
+	LED_on(wall_1_pins, 255, 255, 255)
+	LED_on(wall_2_pins, 255, 255, 255)
+	LED_on(wall_3_pins, 255, 255, 255)
+	LED_on(wall_4_pins, 255, 255, 255)
 
 
+def light_all_color(R, G, B):
+
+	LED_on(wall_1_pins, R, G, B)
+	LED_on(wall_2_pins, R, G, B)
+	LED_on(wall_3_pins, R, G, B)
+	LED_on(wall_4_pins, R, G, B)
+	
 
 def turn_off_all():
+
 	LED_off(wall_1_pins)
 	LED_off(wall_2_pins)
 	LED_off(wall_3_pins)
 	LED_off(wall_4_pins)
 
 
+def blink_color(wait, R, G, B):
 
-def blink():
+	light_all_color(R, G, B)
+	time.sleep(wait)
+	turn_off_all()
+	time.sleep(wait)
+
+
+def blink(wait):
 
 	light_all()
-	time.sleep(1)
-
+	time.sleep(wait)
 	turn_off_all()
-	time.sleep(1)
+	time.sleep(wait)
 
-for i in range(0,250):
-	
+
+def check_align(sensitivity):
+
+        blink_color(1, 255, 0, 0)
+        
+        LED_on(wall_1_pins, 255, 0, 0)
+        if laser_aligned(1, strength):
+                LED_on(wall_1_pins, 0, 255, 0)
+
+        LED_on(wall_2_pins, 255, 0, 0)
+        if laser_aligned(2, strength):
+                LED_on(wall_2_pins, 0, 255, 0)
+
+        LED_on(wall_3_pins, 255, 0, 0)
+        if laser_aligned(3, strength):
+                LED_on(wall_3_pins, 0, 255, 0)
+
+        LED_on(wall_4_pins, 255, 0, 0)
+        if laser_aligned(4, strength):
+                LED_on(wall_4_pins, 0, 255, 0)
+
+        blink_color(1, 0, 255, 0)
+
+        
+
 
 # START GAME
 
-blink()
-blink()
+turn_off_all()
+time.sleep(1)
 
+# check_align(sensitivity)
 
-circuit_len = 60
-sensitivity = 0.1
+blk_wait = 0.5
+blink(blk_wait)
+blink(blk_wait)
+blink(blk_wait)
 
-timeout = time.time() + circuit_len*1   # 1(circuit_len)  minute(s) from now
+circuit_len = 60*len_factor
+timeout = time.time() + circuit_len   # minutes from now
 wall_count = 0
 
-
 print("TIME LEFT: "+str(circuit_len))
+  
+while True:
 
-while time.time() > timeout:
+	if time.time() > timeout:
+		break
 
 	walls = [1,2,3,4]
 	random_wall = random.choice(walls)
-	light_up(random_wall)
+	random_light_up(random_wall)
 
 	if wall_hit(random_wall, sensitivity):
 
+		time_elapsed = time.time() - timeout
+		time_left = 0 - time_elapsed
+		print("TIME LEFT: "+str(time_left))
+
 		wall_count += 1
 		light_off(random_wall)
-		
-		time_elapsed = time.time() - timeout
-		time_left = circuit_len - time_elapsed
-		print("TIME LEFT: "+str(time_left))
-		
 
 light_all()
-print("WALLS HIT: " + wall_count)
+print("WALLS HIT: " + str(wall_count))
